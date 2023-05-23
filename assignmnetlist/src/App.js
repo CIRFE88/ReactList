072826fba11403b6e8110146be45fa17f0ca7e64
Ibraahim Cirfe <ibraahimcirfe@gmail.com>
2023-05-23 14:50:47 +0300

    first commit

---
+++ assignmnetlist/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import DisplayList from "./DisplayList";
+
+
+function App() {
+  return (
+    <div>
+     
+      <DisplayList />
+    </div>
+  );
+}
+
+export default App;
